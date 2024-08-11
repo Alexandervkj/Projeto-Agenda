76a5fb02d6a6e34b2b30cd365fa68407cdfb22ae
ALexander <alexandeer_junior@hotmail.com>
2024-08-11 09:02:44 -0300

    ForeignKey, criando o Model Category e fazendo a ligação com chave estrangeira

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -11,7 +17,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
 
     # metodo para sobreescrever o contatto, ao inves de aparecer Contact(2) aparece seu nome e sobrenome.
     def __str__(self) -> str:
