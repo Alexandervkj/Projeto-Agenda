b62b0d32c48c04f8563eef7b5971a48ac3287255
ALexander <alexandeer_junior@hotmail.com>
2024-08-13 19:43:47 -0300

    Criando o template, a view e a url para exibir contato único

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'last_name', 'phone',
+    list_display = 'id','first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     # list_filter = 'created_date', serve para criar filtros
     search_fields = 'id', 'first_name', 'last_name', 
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'phone',
+    list_editable = 'phone', 'show',
     list_display_links = 'id', 'first_name',
 
 @admin.register(models.Category)
