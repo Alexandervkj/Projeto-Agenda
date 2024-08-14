e37a562f0909efcd4c55c9fb75bd5984c43f44eb
ALexander <alexandeer_junior@hotmail.com>
2024-08-13 21:00:55 -0300

    Criando arquivos parciais para usar com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
 
     context = {
         'contacts' : contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -20,9 +21,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show = True)
     # if single_contact is None:
     #     raise Http404
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact' : single_contact,
+        'site_title' : site_title,
     }
 
     return render(
