18537dadbd97c913b56aec2795e2b20adb72e102
ALexander <alexandeer_junior@hotmail.com>
2024-08-19 22:10:32 -0300

    Validando campos do formulário com clean, clean_field e ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -40,21 +40,35 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        cleaned_data = self.cleaned_data
+        # cleaned_data = self.cleaned_data
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 1',
-                code= 'invalid',
-            ) 
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code= 'invalid',
-            ) 
-        )
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro 1',
+        #         code= 'invalid',
+        #     ) 
+        # )
 
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        # if first_name == 'ABC':
+        #     raise ValidationError(
+        #         'Não digite ABC neste campo',
+        #         code='invalid'
+        #     )
+        
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code= 'invalid',
+                ) 
+            )
+
+        return first_name
+    
