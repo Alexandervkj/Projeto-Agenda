8ecb3d74947a22a23eeb925d1ecbecd92fff8a9d
ALexander <alexandeer_junior@hotmail.com>
2024-08-24 12:28:16 -0300

    Usando UserCreationForm para criar novos usuários no Django

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 from. import models
 
 class ContactForm(forms.ModelForm):
@@ -54,4 +55,7 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
-    
+
+
+class RegisterForm(UserCreationForm):
+    ...
